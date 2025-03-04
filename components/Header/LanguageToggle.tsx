0c7872d004bb8a1dda882368ebd5e80b65264050
Alshaima alaswad <sha98ima@gmail.com>
2025-03-04 02:54:35 +0200

    added language toggle

---
+++ components/Header/LanguageToggle.tsx
@@ -0,0 +1,23 @@
+import { useTranslation } from "react-i18next";
+
+const LanguageToggle = () => {
+  const { i18n } = useTranslation();
+
+  const toggleLanguage = () => {
+    const newLanguage = i18n.language === "en" ? "ar" : "en";
+    i18n.changeLanguage(newLanguage);
+    document.documentElement.dir = newLanguage === "ar" ? "rtl" : "ltr";
+  };
+
+  return (
+    <button
+      aria-label="language toggler"
+      onClick={toggleLanguage}
+      className="bg-gray-2 dark:bg-dark-bg flex cursor-pointer items-center justify-center rounded-full text-black dark:text-white lg:static"
+    >
+      {i18n.language === "en" ? "AR" : "EN"}
+    </button>
+  );
+};
+
+export default LanguageToggle;
